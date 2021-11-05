708dfabeed4ae7a193642cb92724518036de5637
rationalyt <93513715+rationalyt@users.noreply.github.com>
2021-11-05 00:30:00 -0400

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+
+
+class Graph:
+
+    def __init__(self):
+        self.g = defaultdict(list)
+        self.number_edges = 0
+        self.v = set()
+        self.s = 0
+
+    def add_edge(self, u, v):
+        self.g[u].append(v)
+        self.v.add(u)
+        self.v.add(v)
+        self.number_edges = self.number_edges + 1
+
+    def bfs(self, s):
+        self.s = s
+        queue = []
+        visited = defaultdict(int)
+        visited[s] = 1
+        queue.append(s)
+        while queue:
+            expand_v = queue.pop(0)
+            print(expand_v, end=" ")
+            for i in self.g[expand_v]:
+                if visited[i] != 1:
+                    queue.append(i)
+                    visited[i] = 1
+
+    def dfs(self, s):
+        self.s = s
+        visited = defaultdict(int)
+        visited[s] = 1
+        self.dfs_util(s, visited)
+        for i in self.v:
+            if visited[i] != 1:
+                self.dfs_util(i, visited)
+
+    def dfs_util(self, s, visited):
+        self.s = s
+        stack = []
+        stack.append(s)
+        while stack:
+            expand_v = stack.pop()
+            print(expand_v, end=" ")
+            for i in self.g[expand_v]:
+                if visited[i] != 1:
+                    stack.append(i)
+                    visited[i] = 1
+
+
---
+++ graph_implement.py
@@ -0,0 +1,23 @@
+from graph import Graph
+graph = Graph()
+print("Enter the edges of graph in u->v format and enter stop when done:\n")
+while True:
+    k = input()
+    if k.lower() == "stop":
+        break
+    u, v = int(k.split("->")[0]), int(k.split("->")[1])
+    graph.add_edge(u, v)
+
+print("\nThe vertices in Graph are:")
+for i in graph.v:
+    print(i, end=" ")
+print(f"\nThe graph has {graph.number_edges} edges")
+print("\n\nSelect the source vertex for Breadth First Search Traversal:")
+k = int(input())
+print("\nThe BFS traversal is:")
+graph.bfs(k)
+print("\n\nSelect the source vertex for Depth First Search Traversal:")
+k = int(input())
+print("\nThe DFS traversal is:")
+graph.dfs(k)
+
